95a4b501438e29b28dbb4ad85170615288f7d01b
absolutetsonga <abstsonga@gmail.com>
2023-07-15 13:40:21 +0600

    modified: theme switch provider

--- app/layout.js
+++ app/layout.js
@@ -1,5 +1,8 @@
-import './globals.css';
 import { Poppins } from 'next/font/google';
+import './globals.css';
+
+import AuthProvider from './components/providers/AuthProvider';
+import ThemeSwitchProvider from './components/providers/ThemeSwitchProvider';
 
 const poppins = Poppins({ weight: '400', subsets: ['latin'] });
 
@@ -11,7 +14,11 @@ export const metadata = {
 export default function RootLayout({ children }) {
     return (
         <html lang="en">
-            <body className={poppins.className}>{children}</body>
+            <ThemeSwitchProvider>
+                <AuthProvider>
+                    <body className={poppins.className}>{children}</body>
+                </AuthProvider>
+            </ThemeSwitchProvider>
         </html>
     );
 }
