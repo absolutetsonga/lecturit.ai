e856130c96caaf22e5c134ab01fa873eb032d1a2
absolutetsonga <abstsonga@gmail.com>
2023-07-15 22:01:14 +0600

    modified: all functions are in useEffect now

--- app/pages/new-summary/page.jsx
+++ app/pages/new-summary/page.jsx
@@ -14,9 +14,13 @@ const newSummary = () => {
 
     const [microphoneAccess, setMicrophoneAccess] = useState(false);
     const [isRecordingAudio, setIsRecordingAudio] = useState(false);
-    const [recordedAudio, setRecorderAudio] = useState();
+    const [recordedAudio, setRecordedAudio] = useState();
+
+    const [transcribedText, setTranscribedText] = useState('');
+    const [summaryText, setSummaryText] = useState('');
 
     const mediaRecorderRef = useRef();
+    const audioRef = useRef();
 
     useEffect(() => {
         const handleMicrophoneAccess = async () => {
@@ -38,79 +42,89 @@ const newSummary = () => {
         };
 
         handleMicrophoneAccess();
+
+        const getMediaRecorder = async () => {
+            try {
+                const mediaRecorder = new MediaRecorder(stream);
+                mediaRecorderRef.current = mediaRecorder;
+
+                mediaRecorder.addEventListener(
+                    'dataavailable',
+                    async (event) => {
+                        const formData = await handleDataAvailable(event);
+                        const transcriptedText = await getTranscript(formData);
+                        const summary = await addSummary(transcriptedText);
+
+                        return summary;
+                    },
+                );
+            } catch (error) {
+                console.error(
+                    `Error while trying to get media recorder. Error message: ${err.message}`,
+                );
+            }
+        };
+
+        const handleDataAvailable = async (event) => {
+            try {
+                const audioBlob = event.data;
+                const audioURL = URL.createObjectURL(audioBlob);
+
+                setRecordedAudio(audioURL);
+
+                const audioFile = new File([audioBlob], 'audio.mp3', {
+                    type: 'audio/mp3',
+                });
+
+                const formData = new FormData();
+                formData.append('file', audioFile);
+                formData.append('model', 'whisper-1');
+
+                return formData;
+            } catch (error) {
+                console.error(
+                    `Error while trying to handle available data from media recorder. Error message: ${error.message}`,
+                );
+            }
+        };
+
+        const getTranscript = async (formData) => {
+            try {
+                const { data: transcriptedText } = await axios.post(
+                    '/api/whisper',
+                    formData,
+                );
+
+                const { text } = transcriptedText;
+
+                setTranscribedText(text);
+
+                return text;
+            } catch (error) {
+                console.error(
+                    `Error while trying to get response from Whisper. Error message: ${error.message}`,
+                );
+            }
+        };
+
+        const addSummary = async (transcript) => {
+            try {
+                const summary = await axios.post('/api/summary/new', {
+                    userId: session?.user.id,
+                    summary: JSON.stringify(transcript),
+                });
+
+                console.log(summary.data.summary);
+                setSummaryText(summary.data.summary);
+
+                return summaryText;
+            } catch (error) {
+                console.error(
+                    `Error while trying to add summary. Error message: ${error.message}`,
+                );
+            }
+        };
     }, []);
-
-    const getMediaRecorder = async () => {
-        try {
-            const mediaRecorder = new MediaRecorder(stream);
-            mediaRecorderRef.current = mediaRecorder;
-
-            mediaRecorder.addEventListener('dataavailable', async (event) => {
-                const formData = await handleDataAvailable(event);
-                const transcript = await getTranscript(formData);
-                const summary = await addSummary(transcript.text);
-
-                return summary;
-            });
-        } catch (error) {
-            console.error(
-                `Error while trying to get media recorder. Error message: ${err.message}`,
-            );
-        }
-    };
-
-    const handleDataAvailable = async (event) => {
-        try {
-            const audioBlob = event.data;
-            const audioURL = URL.createObjectURL(audioBlob);
-
-            setRecorderAudio(audioURL);
-
-            const audioFile = new File([audioBlob], 'audio.mp3', {
-                type: 'audio/mp3',
-            });
-
-            const formData = new FormData();
-            formData.append('file', audioFile);
-            formData.append('model', 'whisper-1');
-
-            return formData;
-        } catch (error) {
-            console.error(
-                `Error while trying to handle available data from media recorder. Error message: ${error.message}`,
-            );
-        }
-    };
-
-    const getTranscript = async (formData) => {
-        try {
-            const { data: transcript } = await axios.post(
-                '/api/whisper',
-                formData,
-            );
-        
-            return transcript;
-        } catch (error) {
-            console.error(
-                `Error while trying to get response from Whisper. Error message: ${error.message}`,
-            );
-        }
-    };
-
-    const addSummary = async (transcript) => {        
-        try {
-            const responseBackend = await axios.post('/api/summary/new', {
-                userId: session?.user.id,
-                summary: JSON.stringify(transcript),
-            });
-
-            return responseBackend;
-        } catch (error) {
-            console.error(
-                `Error while trying to add summary. Error message: ${error.message}`,
-            );
-        }
-    };
 
     const handleRecordClick = () => {
         if (isRecordingAudio) {
@@ -122,41 +136,79 @@ const newSummary = () => {
 
             if (mediaRecorder) {
                 mediaRecorder.start();
-                // setRecordedAudio(null);
+                setRecordedAudio(null);
             }
         }
 
         setIsRecordingAudio((prev) => !prev);
     };
-
+    
     if (status === 'loading') return <> Loading </>;
     return (
-        <div>
-            <button onClick={handleRecordClick}>
-                {isRecordingAudio ? (
+        <div className="container">
+            {microphoneAccess ? (
+                <>
+                    <button onClick={handleRecordClick}>
+                        {isRecordingAudio ? (
+                            <div
+                                className={`${
+                                    theme === 'dark'
+                                        ? 'bg-pause-light'
+                                        : 'bg-pause-dark'
+                                } h-[50px] w-[50px] bg-no-repeat object-cover transition-all`}
+                            />
+                        ) : (
+                            <div
+                                className={`${
+                                    theme === 'dark'
+                                        ? 'bg-play-light'
+                                        : 'bg-play-dark'
+                                } h-[50px] w-[50px] bg-no-repeat object-cover transition-all`}
+                            />
+                        )}
+                    </button>
+
+                    {recordedAudio && (
+                        <div>
+                            <p> Recorded Audio: </p>
+                            <audio
+                                ref={audioRef}
+                                controls
+                                src={recordedAudio}
+                                type="audio/mpeg"
+                            />
+                        </div>
+                    )}
+
+                    <div className="flex flex-col items-center gap-3">
+                        <div className="flex flex-row items-center gap-3">
+                            <div
+                                className={`${
+                                    theme === 'dark'
+                                        ? 'bg-mic-light'
+                                        : 'bg-mic-dark'
+                                } h-[50px] w-[50px] bg-no-repeat object-cover transition-all`}
+                            />
+
+                            <p> Microphone access granted </p>
+                        </div>
+
+                        <p>{transcribedText}</p>
+                        <p>{summaryText}</p>
+                    </div>
+                </>
+            ) : (
+                <div>
                     <div
                         className={`${
                             theme === 'dark'
-                                ? 'bg-pause-light'
-                                : 'bg-pause-dark'
+                                ? 'bg-mic-no-light'
+                                : 'bg-mic-no-dark'
                         } h-[50px] w-[50px] bg-no-repeat object-cover transition-all`}
                     />
-                ) : (
-                    <div
-                        className={`${
-                            theme === 'dark' ? 'bg-play-light' : 'bg-play-dark'
-                        } h-[50px] w-[50px] bg-no-repeat object-cover transition-all`}
-                    />
-                )}
-            </button>
-
-            {microphoneAccess ? (
-                <p> Yes microphone Access </p>
-            ) : (
-                <p> No microphone Access </p>
+                    <p> Microphone access denied </p>
+                </div>
             )}
-
-            {/* {<button onClick={getMediaRecorder}> getMediaRecorder </button>} */}
         </div>
     );
 };
