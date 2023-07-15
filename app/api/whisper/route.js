4f70ea14e36730285b7d86b6ab1e751c27eaed98
absolutetsonga <abstsonga@gmail.com>
2023-07-15 17:48:29 +0600

    new: route for requesting whisper api model

---
+++ app/api/whisper/route.js
@@ -0,0 +1,25 @@
+import { connectToDb } from '@/utils/database';
+import { NextResponse } from 'next/server';
+import axios from 'axios';
+
+export const POST = async (req) => {
+    const formData = await req.formData();
+
+    try {
+        const { data } = await axios.post(
+            'https://api.openai.com/v1/audio/transcriptions',
+            formData,
+            {
+                headers: {
+                    Authorization: `Bearer ${process.env.OPENAI_API_KEY}`,
+                },
+            },
+        );
+
+        return NextResponse.json(data);
+    } catch (error) {
+        console.error(
+            `Error while trying to send request to the Whisper API: ${error.message}`,
+        );
+    }
+};
