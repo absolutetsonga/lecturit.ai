faf02cb1a40882d46d5921f2500c1ee014072ab8
absolutetsonga <abstsonga@gmail.com>
2023-07-18 12:02:21 +0600

    new: utility functions for converting text to formatted parts of the summary

---
+++ utils/getStructuredSubarrays.js
@@ -0,0 +1,23 @@
+const getStructuredSubarrays = (summary) => {
+    const sentences = summary.split('. ');
+
+    let subarraysOfSentences = [];
+    let currentSubarray = [];
+
+    for (let i = 0; i <= sentences.length; i++) {
+        if (i !== 0 && i % 10 === 0) {
+            currentSubarray.push(sentences[i]);
+            subarraysOfSentences.push(currentSubarray.join('. '));
+            currentSubarray = [];
+        } else {
+            currentSubarray.push(sentences[i]);
+        }
+    }
+
+    return subarraysOfSentences;
+};
+
+export default getStructuredSubarrays;
+
+// input: sentences variable - array of the strings divided by dot and space (. )
+// output: two dimensional array each element of the array is 10 sentences.
