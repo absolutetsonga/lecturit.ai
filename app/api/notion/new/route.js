39d0ccef8452dba585a72b309fe082cdfa08d7d0
absolutetsonga <abstsonga@gmail.com>
2023-07-18 18:49:43 +0600

    modified: rich text now includes dynamic separate text paragraphs

--- app/api/notion/new/route.js
+++ app/api/notion/new/route.js
@@ -2,54 +2,55 @@ import { connectToDb } from '@/utils/database';
 import { Client } from '@notionhq/client';
 
 export const POST = async (req) => {
-    const { summary } = await req.json();
+    const { formattedResults } = await req.json();
 
-    console.log(summary);
-    
     try {
+        const formattedResultsArray = JSON.parse(formattedResults);
+
         await connectToDb();
 
         const notion = new Client({ auth: process.env.NOTION_API_KEY });
-        let response;
 
-        if (notion) {
-            const createSummary = async () => {
-                response = await notion.pages.create({
-                    parent: {
-                        type: 'database_id',
-                        database_id: process.env.NOTION_DATABASE_ID,
-                    },
-                    properties: {
-                        Name: {
-                            title: [
-                                {
-                                    text: {
-                                        content:
-                                            'Andrew Huberman Podcast about Fitness and Food',
-                                    },
+        if (!notion) return;
+
+        const richTextArray = formattedResultsArray.map((el) => ({
+            text: {
+                content: el,
+            },
+        }));
+
+        const createSummary = async () => {
+            const response = await notion.pages.create({
+                parent: {
+                    type: 'page_id',
+                    page_id: process.env.NOTION_PAGE_ID,
+                },
+                properties: {
+                    title: {
+                        title: [
+                            {
+                                text: {
+                                    content:
+                                        'Andrew Huberman Podcast about Fitness and Food',
                                 },
-                            ],
+                            },
+                        ],
+                    },
+                },
+                children: [
+                    {
+                        object: 'block',
+                        paragraph: {
+                            rich_text: richTextArray,
                         },
                     },
-                    children: [
-                        {
-                            object: 'block',
-                            paragraph: {
-                                rich_text: [
-                                    {
-                                        text: {
-                                            content: summary,
-                                        },
-                                    },
-                                ],
-                            },
-                        },
-                    ],
-                });
-            };
+                ],
+            });
 
-            createSummary();
-        }
+            return response;
+        };
+
+        const response = createSummary();
 
         return new Response(JSON.stringify(response), {
             status: 201,
