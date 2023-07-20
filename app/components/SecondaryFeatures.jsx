106e235e050a1c504734a301b24e5f839a7d83cb
absolutetsonga <abstsonga@gmail.com>
2023-07-20 14:56:57 +0600

    modified: background color for Tab Panels

--- app/components/SecondaryFeatures.jsx
+++ app/components/SecondaryFeatures.jsx
@@ -186,7 +186,7 @@ function FeaturesDesktop() {
                             />
                         ))}
                     </Tab.List>
-                    <Tab.Panels className="relative mt-20 overflow-hidden rounded-4xl bg-slate-700 px-14 py-16 xl:px-16">
+                    <Tab.Panels className="relative mt-20 overflow-hidden rounded-4xl bg-slate-200 px-14 py-16 dark:bg-slate-700 xl:px-16">
                         <div className="-mx-5 flex">
                             {features.map((feature, featureIndex) => (
                                 <Tab.Panel
