8a0fd1cf53517f51ba61f003a6c2f7513c8a1045
absolutetsonga <abstsonga@gmail.com>
2023-07-20 16:21:16 +0600

    new: CTA component

--- app/page.jsx
+++ app/page.jsx
@@ -1,6 +1,7 @@
 import { Hero } from '@/app/components/Hero';
 import { PrimaryFeatures } from '@/app/components/PrimaryFeatures';
 import { SecondaryFeatures } from './components/SecondaryFeatures';
+import { CTA } from './components/CTA';
 
 export default function Home() {
     return (
@@ -8,6 +9,7 @@ export default function Home() {
             <Hero />
             <PrimaryFeatures />
             <SecondaryFeatures />
+            <CTA/>
         </div>
     );
 }
