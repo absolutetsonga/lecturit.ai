5dafc8c1cd7bf6916499025158b3394b4b182b75
absolutetsonga <abstsonga@gmail.com>
2023-07-20 11:57:29 +0600

    modified: removed toggle switcher, now it toggles on image click

--- app/components/navbar/ThemeSwitcher.jsx
+++ app/components/navbar/ThemeSwitcher.jsx
@@ -30,28 +30,16 @@ const ThemeSwitcher = () => {
 
     return (
         <div className="flex items-center">
-            <div
-                className="relative inline-flex cursor-pointer items-center"
-                onClick={toggleTheme}
-            >
-                <input
-                    type="checkbox"
-                    value=""
-                    className="peer sr-only"
-                    checked={theme === 'dark' ? true : false}
-                    ref={checkboxRef}
-                />
-                <div className="peer h-10 w-20 rounded-full bg-gray-200 after:absolute after:left-[4px] after:top-[2px] after:h-9 after:w-9 after:rounded-full after:border after:border-gray-300 after:bg-white after:transition-all after:content-[''] peer-checked:bg-blue-600 peer-checked:after:translate-x-full peer-checked:after:border-white peer-focus:outline-none peer-focus:ring-4 peer-focus:ring-blue-300 dark:border-gray-600 dark:bg-gray-700 dark:peer-focus:ring-blue-800"></div>
-            </div>
             <button
                 id="theme-toggle"
                 type="button"
                 className="rounded-lg p-2.5 text-sm text-gray-500 hover:bg-gray-100 focus:outline-none focus:ring-4 focus:ring-gray-200 dark:text-gray-400 dark:hover:bg-gray-700 dark:focus:ring-gray-700"
+                onClick={toggleTheme}
             >
                 <div
                     className={`${
                         theme === 'dark' ? 'bg-moon-dark' : 'bg-sun-light'
-                    } h-[50px] w-[50px] bg-no-repeat object-cover transition-all`}
+                    } h-[35px] w-[35px] bg-no-repeat object-cover transition-all`}
                 />
             </button>
         </div>
