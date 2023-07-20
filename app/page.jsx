2ec939c66c8217185bee3ac92f2ee7c95fa16c8a
absolutetsonga <abstsonga@gmail.com>
2023-07-20 14:55:55 +0600

    new: secondary feature component

--- app/page.jsx
+++ app/page.jsx
@@ -1,11 +1,13 @@
 import { Hero } from '@/app/components/Hero';
 import { PrimaryFeatures } from '@/app/components/PrimaryFeatures';
+import { SecondaryFeatures } from './components/SecondaryFeatures';
 
 export default function Home() {
     return (
         <div>
             <Hero />
             <PrimaryFeatures />
+            <SecondaryFeatures />
         </div>
     );
 }
