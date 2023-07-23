014840604ae173f7f8f3065ffeb2ea4be922b4c8
absolutetsonga <abstsonga@gmail.com>
2023-07-23 16:13:17 +0600

    new: TabDivider divides RecordAudio and UploadFile components

---
+++ app/components/TabDivider.jsx
@@ -0,0 +1,42 @@
+export const TabDivider = ({ links, setLinks }) => {
+    const handleLinkClick = (clickedLabel) => {
+        const updatedLinks = links.map((link) =>
+            link.label === clickedLabel
+                ? { ...link, active: true }
+                : { ...link, active: false },
+        );
+
+        setLinks(updatedLinks);
+    };
+
+    return (
+        <div className="w-[60%] border-b border-gray-200 px-8 dark:border-gray-700">
+            <ul className="-mb-px flex flex-wrap text-center text-sm font-medium text-gray-500 dark:text-gray-400">
+                {links.map((link, index) => {
+                    console.log({ name: link.label, active: link.active });
+
+                    return (
+                        <li
+                            className="mr-2"
+                            onClick={() => handleLinkClick(link.label)}
+                            key={index}
+                        >
+                            <a
+                                className={`${
+                                    link.active
+                                        ? 'border-b-2 text-lg font-bold'
+                                        : 'font-normal'
+                                }  group inline-flex items-center justify-center gap-3 rounded-t-lg  border-transparent p-4 hover:border-gray-300 hover:text-gray-600 dark:hover:text-gray-300`}
+                            >
+                                <div className="h-[30px] w-[30px]">
+                                    {link.icon}
+                                </div>
+                                {link.label}
+                            </a>
+                        </li>
+                    );
+                })}
+            </ul>
+        </div>
+    );
+};
